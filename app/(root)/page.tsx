98e688e6e38a4cdd822c57801c429d9fc162ee2f
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-02-13 22:07:10 -0400

    feat(layout): update import paths for Navbar and add SearchForm to Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,23 @@
-export default function Home() {
+import { SearchForm } from "@components/search-form";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch Your Startup, <br /> 
+          Pitch Your Startup, <br />
           Connect with Investors
         </h1>
         <p className="sub-heading !max-w-3xl">
-
-          Here you can present your innovative ideas and capture the attention of potential investors.
+          Here you can present your innovative ideas and capture the attention
+          of potential investors.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
